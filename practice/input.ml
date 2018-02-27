cd5c9d4bc7487fcc5a5f0aa04c534d3dfbece5bc
Guillaume Bury <guillaume.bury@gmail.com>
2018-02-27 15:40:52 +0100

    Add analyze mode

--- practice/input.ml
+++ practice/input.ml
@@ -1,33 +1,34 @@
 
-let read_int () = Scanf.scanf " %d " (fun x -> x)
+let read_int ic = Scanf.bscanf ic " %d " (fun x -> x)
 
-let read_rect () =
-  let a1 = read_int () in
-  let a2 = read_int () in
-  let a3 = read_int () in
-  let a4 = read_int () in
+let read_rect ic =
+  let a1 = read_int ic in
+  let a2 = read_int ic in
+  let a3 = read_int ic in
+  let a4 = read_int ic in
   { State.tl = { State.r = a1 ; c = a2 };
     br = { State.r=a3 ; c = a4 } }
 
-let read_cell i j = Scanf.scanf " %c " (function
+let read_cell ic i j =
+  Scanf.bscanf ic " %c " (function
     | 'T' -> State.T
     | 'M' -> State.M
     | c -> Format.eprintf "read '%c' at %d, %d@." c i j; assert false)
 
-let read_state () =
-  let r = read_int () in
-  let c = read_int () in
-  let l = read_int () in
-  let h = read_int () in
+let read_state ic =
+  let r = read_int ic in
+  let c = read_int ic in
+  let l = read_int ic in
+  let h = read_int ic in
   let pizza = Array.init r (fun i ->
-      Array.init c (fun j -> read_cell i j)) in
+      Array.init c (fun j -> read_cell ic i j)) in
   State.mk l h pizza
 
-let read_solution () =
-  let s = read_int () in
+let read_solution ic =
+  let s = read_int ic in
   let l = ref [] in
     for i=1 to s do
-      l := read_rect ()::!l
+      l := read_rect ic :: !l
     done;
   !l
 
--- practice/main.ml
+++ practice/main.ml
@@ -1,7 +1,16 @@
 
 let () =
-  let state = Input.read_state () in
-  let res = Dyn.dyn_dyn state 4 in
-  Format.eprintf "score: %d@." (State.score res);
-  Format.printf "%a@." State.pp_solution res
+  if Array.length Sys.argv > 2 then begin
+    (* Analyze mode *)
+    let state = Input.read_state (Scanf.Scanning.open_in Sys.argv.(1)) in
+    let sol = Input.read_solution (Scanf.Scanning.open_in Sys.argv.(2)) in
+    let unused = Solutionreader.notsliced sol state.State.pizza in
+    Format.printf "@[<hv>unused points:@ %a@]"
+      CCFormat.(list ~sep:(return "@ ") (pair int int)) unused
+  end else begin
+    let state = Input.read_state Scanf.Scanning.stdin in
+    let res = Dyn.dyn_dyn state 4 in
+    Format.eprintf "score: %d@." (State.score res);
+    Format.printf "%a@." State.pp_solution res
+  end
 
