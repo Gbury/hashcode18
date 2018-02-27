de341fff4d9838691cbb187ca5690947ce84b4f3
Guillaume Bury <guillaume.bury@gmail.com>
2018-02-27 15:48:21 +0100

    Add support for analyze mode in makefile

--- practice/main.ml
+++ practice/main.ml
@@ -5,8 +5,8 @@ let () =
     let state = Input.read_state (Scanf.Scanning.open_in Sys.argv.(1)) in
     let sol = Input.read_solution (Scanf.Scanning.open_in Sys.argv.(2)) in
     let unused = Solutionreader.notsliced sol state.State.pizza in
-    Format.printf "@[<hv>unused points:@ %a@]"
-      CCFormat.(list ~sep:(return "@ ") (pair int int)) unused
+    Format.printf "@[<v>unused points:@ %a@]@."
+      CCFormat.(list ~sep:(return "@ ") (hovbox @@ pair int int)) unused
   end else begin
     let state = Input.read_state Scanf.Scanning.stdin in
     let res = Dyn.dyn_dyn state 4 in
