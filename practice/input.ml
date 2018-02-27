c733125d597a03d2a25264132f672d2369d99a28
Eliot Pacherie <eliot.pacherie@gmail.com>
2018-02-27 09:27:11 -0500

    lol

--- practice/input.ml
+++ practice/input.ml
@@ -1,6 +1,14 @@
 
 let read_int () = Scanf.scanf " %d " (fun x -> x)
 
+let read_rect () =
+  let a1 = read_int () in
+  let a2 = read_int () in
+  let a3 = read_int () in
+  let a4 = read_int () in
+  { State.tl = { State.r = a1 ; c = a2 };
+    br = { State.r=a3 ; c = a4 } }
+
 let read_cell i j = Scanf.scanf " %c " (function
     | 'T' -> State.T
     | 'M' -> State.M
@@ -14,5 +22,14 @@ let read_state () =
   let pizza = Array.init r (fun i ->
       Array.init c (fun j -> read_cell i j)) in
   State.mk l h pizza
+
+let read_solution () =
+  let s = read_int () in
+  let l = ref [] in
+    for i=1 to s do
+      l := read_rect ()::!l
+    done;
+  !l
+
 
 
