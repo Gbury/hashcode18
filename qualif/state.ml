5cbeab56deac1742d23dfd974e6519436307f419
Ekdohibs <nathanael.courant@laposte.net>
2018-03-01 20:23:12 +0100

    finish_time

--- qualif/state.ml
+++ qualif/state.ml
@@ -10,6 +10,10 @@ type t = {
 
 let mk f b t r c rides = { f; b; t; r; c; rides; }
 
+let finish_time t ride time point =
+  let d = Point.dist point ride.Ride.start in
+  (max (time + d) ride.Ride.first) + ride.Ride.dist
+
 let score t ride time point =
   let d = Point.dist point ride.Ride.start in
   let bonus = if time + d <= ride.Ride.first then t.b else 0 in
